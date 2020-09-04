f5ccf4587ea0fab18f325ec970674a2084e30e6c
Huynh Du Luong <huynhduluong@gmail.com>
2020-09-04 16:44:16 +0700

    create class TaxiList

---
+++ js/TaxiList.js
@@ -0,0 +1,29 @@
+/**
+ * Mục đích: Tạo mảng chứa các loại taxi
+ * Người tạo: Lượng
+ * Ngày tạo: 4/9/2020
+ * Version: 1.0
+ */
+var taxiArray = [
+    {
+        taxiType: "uberX",
+        theFirstKM: 8000,
+        from1To20KM: 12000,
+        higher21KM: 10000,
+        waitingFee: 2000
+    },
+    {
+        taxiType: "uberSUV",
+        theFirstKM: 9000,
+        from1To20KM: 14000,
+        higher21KM: 12000,
+        waitingFee: 3000
+    },
+    {
+        taxiType: "uberBlack",
+        theFirstKM: 10000,
+        from1To20KM: 16000,
+        higher21KM: 14000,
+        waitingFee: 4000
+    }
+];
